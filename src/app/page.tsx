0f3d41d9959a521e8ac28753c988906f12d62a59
develiaskh <develiaskh@gmail.com>
2025-12-31 16:36:42 +0330

    ✏️ feat: add mobile version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import NavBar from "@/components/NavBar";
+import NavBar from '@/components/NavBar';
 
-
-
-import NAV_BAR_LINK from "@/constants/NavBar";
+import NAV_BAR_LINK from '@/constants/NavBar';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <NavBar links={NAV_BAR_LINK} />
+    <div className='lg:px-20'>
+      <nav className='flex items-center justify-between'>
+        <div className='logo sm:basis-1/3'>
+          {/* logo here */}
+          <span>DevEliasKh</span>
+        </div>
+        <NavBar links={NAV_BAR_LINK} className='basis-2/3' />
+      </nav>
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,47 +1,64 @@
-import React from 'react'
-import NavBarLinks from '@/types'
-import Link from 'next/link'
+'use client';
+import { useState } from 'react';
+import React from 'react';
+import NavBarLinks from '@/types';
+import Link from 'next/link';
 import Image from 'next/image';
-import downloadIcon from '@/assets/icon/download.svg'
+import downloadIcon from '@/assets/icon/download.svg';
+import menuIcon from '@/assets/icon/menu.svg';
 
 interface NavBarProps {
-  links: NavBarLinks
+  links: NavBarLinks;
 }
 
 const NavBar: React.FC<NavBarProps> = ({ links }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  function toggleMobileMenu() {
+    // Function to toggle mobile menu visibility
+    setIsMobileMenuOpen(() => !isMobileMenuOpen);
+  }
+
   return (
-    <div className='flex justify-between items-center w-full'>
+    <>
+      <div className='hidden w-full basis-2/3 items-center justify-between sm:flex'>
+        <div className='links'>
+          <ul className='flex gap-3 font-semibold'>
+            {links.map((item) => (
+              <li key={item.link + item.name}>
+                <Link href={`#${item.link}`}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className="logo">
-        {/* logo here */}
-        <span>DevEliasKh</span>
-      </div>
-      <div className="links">
-        <ul className='flex gap-3 font-semibold'>
-
-          {links.map((item) => (
-            <li key={item.link + item.name}>
-              <Link
-                href={`#${item.link}`}
-              >
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <button className='resume flex gap-2 rounded bg-black px-2 py-2 text-white'>
+          <span>Resume</span>
+          <Image priority src={downloadIcon} alt='download resume' />
+        </button>
       </div>
 
-      <button className='bg-black resume flex gap-2 px-2 py-2 rounded text-white'>
-        <span>Resume</span>
-        <Image
-          priority
-          src={downloadIcon}
-          alt='download resume'
-        />
-      </button>
+      {/* Humber menu in mobile  */}
+      <div className='sm:hidden'>
+        <Image priority src={menuIcon} alt='menu' onClick={toggleMobileMenu} />
+        {isMobileMenuOpen && (
+          <div className='absolute left-0 top-16 w-full bg-white/90 p-4 backdrop-blur-md'>
+            <ul className='flex flex-col gap-4 font-semibold'>
+              {links.map((item) => (
+                <li key={item.link + item.name}>
+                  <Link href={`#${item.link}`}>{item.name}</Link>
+                </li>
+              ))}
+              <button className='resume flex gap-2 rounded bg-black px-2 py-2 text-white'>
+                <span>Resume</span>
+                <Image priority src={downloadIcon} alt='download resume' />
+              </button>
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
-    </div>
-  )
-}
-
-export default NavBar
+export default NavBar;
